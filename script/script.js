fe57f2bfc0f96643977aef1354af5c405dfbca5c
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2022-10-08 11:14:30 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -4,3 +4,35 @@ sr.reveal('.container_projetos, #sobre_mim, #formulario, #projetos h1, #sistema 
     rotate:{x:0, y:10, z: 0} 
 });
 
+
+
+function emBreve(){
+    window.alert("Em Breve!");
+}
+
+
+let inputNome = document.getElementById("name");
+let inputEmail =  document.getElementById("email");
+let inputMensagem =  document.getElementById("message");
+
+let envio = document.getElementById("envio");
+let contato = document.getElementById("contato");
+
+
+function enviar(){
+    let nome = inputNome.value;
+    let email = inputEmail.value;
+    let mensagem = inputMensagem.value;
+
+    
+
+    if(nome==''|| email==''||mensagem==''){
+        window.alert("Preencha todas as áreas");
+        envio.value = "";   
+        contato.action = "";
+        contato.method = "";
+    }
+    else{
+        window.alert("Eviado! Aguarde o retorno.");
+}
+}
